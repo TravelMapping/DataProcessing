a12a054e7303e7caef606d6e5755479448f55c29
jteresco <terescoj@gmail.com>
2018-06-16 22:05:39 -0400

    Minor updates to local site update script.

--- siteupdate/python-teresco/localupdate.sh
+++ siteupdate/python-teresco/localupdate.sh
@@ -74,11 +74,13 @@ echo "$0: switching to primary DB"
 ln -sf $tmwebbase/lib/tm.conf.standard $tmwebbase/lib/tm.conf
 echo "$0: loading DB copy"
 mysql --defaults-group-suffix=tmapadmin -u travmapadmin TravelMappingCopy < TravelMapping-$datestr.sql
+echo "$0: moving sql file to archive"
+mv TravelMapping-$datestr.sql $tmpdir
+echo "$0: sending email notification"
+mailx -s "Travel Mapping Site Update Complete" travelmapping-siteupdates@teresco.org <<EOF
+A Travel Mapping site update has just successfully completed.
+The complete log is available at http://travelmapping.net/logs/siteupdate.log .
+Please report any problems to travmap@teresco.org .
+EOF
 echo "$0: complete"
-#echo "$0: sending email notification"
-#mailx -s "Travel Mapping Site Update Complete" travelmapping-siteupdates@teresco.org <<EOF
-#A Travel Mapping site update has just successfully completed.
-#The complete log is available at http://tm.teresco.org/logs/siteupdate.log .
-#Please report any problems to travmap@teresco.org .
-#EOF
 date
