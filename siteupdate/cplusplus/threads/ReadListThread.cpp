414dbede657d09b15ed040c032ad34c0d127840f
Eric Bryant <ericdbryant@yahoo.com>
2019-03-11 18:43:01 -0400

    upload threads/

---
+++ siteupdate/cplusplus/threads/ReadListThread.cpp
@@ -0,0 +1,18 @@
+void ReadListThread(std::list<std::string> *traveler_ids, std::list<TravelerList*> *traveler_lists, std::mutex *tl_mtx, std::mutex *strtok_mtx, Arguments *args, std::unordered_map<std::string, Route*> *route_hash)
+{	//std::cout << "Starting ReadListThread " << id << std::endl;
+	while (traveler_ids->size())
+	{	tl_mtx->lock();
+		if (!traveler_ids->size())
+		{	tl_mtx->unlock();
+			return;
+		}
+		//std::cout << "Thread " << id << " with traveler_ids.size()=" << traveler_ids.size() << std::endl;
+		std::string tl(traveler_ids->front());
+		//std::cout << "Thread " << id << " assigned " << tl << std::endl;
+		traveler_ids->pop_front();
+		//std::cout << "Thread " << id << " traveler_ids->pop_front() successful." << std::endl;
+		std::cout << ' ' << tl << std::flush;
+		tl_mtx->unlock();
+		traveler_lists->push_back(new TravelerList(tl, route_hash, args, strtok_mtx));
+	}
+}
---
+++ siteupdate/cplusplus/threads/SubgraphThread.cpp
@@ -0,0 +1,16 @@
+void SubgraphThread(unsigned int id, HighwayGraph *graph_data, std::vector<GraphListEntry> *graph_vector, size_t *index, std::mutex *mtx, std::string path)
+{	//std::cout << "Starting SubgraphThread " << id << std::endl;
+	while (*index < graph_vector->size())
+	{	mtx->lock();
+		if (*index >= graph_vector->size())
+		{	mtx->unlock();
+			return;
+		}
+		//std::cout << "Thread " << id << " with graph_vector.size()=" << graph_vector->size() << " & index=" << *index << std::endl;
+		//std::cout << "Thread " << id << " assigned " << graph_vector->at(*index).tag() << std::endl;
+		size_t i = *index;
+		*index += 2;
+		mtx->unlock();
+		graph_data->write_subgraphs_tmg(*graph_vector, path, i, id);
+	}
+}
