b5f4c6c116fd1f4c47dcf3126fc8261a86e104bf
Eric Bryant <ericdbryant@yahoo.com>
2019-03-12 18:49:54 -0400

    graph threads: receive & pass traveler_lists

--- siteupdate/cplusplus/threads/MasterTmgThreads.cpp
+++ siteupdate/cplusplus/threads/MasterTmgThreads.cpp
@@ -2,6 +2,6 @@ void MasterTmgSimpleThread(HighwayGraph *graph_data, GraphListEntry *msptr, std:
 {	graph_data->write_master_tmg_simple(msptr, filename);
 }
 
-void MasterTmgCollapsedThread(HighwayGraph *graph_data, GraphListEntry *mcptr, std::string filename)
-{	graph_data->write_master_tmg_collapsed(mcptr, filename, 1);
+void MasterTmgCollapsedThread(HighwayGraph *graph_data, GraphListEntry *mcptr, std::string filename, std::list<TravelerList*> *traveler_lists)
+{	graph_data->write_master_tmg_collapsed(mcptr, filename, 1, traveler_lists);
 }
