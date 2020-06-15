c878b32617bff601333bcbc66bbd8b737389f0d9
eric bryant <ericdbryant@yahoo.com>
2020-06-15 16:42:00 -0400

    DUPLICATE_LABEL userlog warning bugfix

--- siteupdate/python-teresco/siteupdate.py
+++ siteupdate/python-teresco/siteupdate.py
@@ -2922,10 +2922,10 @@ for h in highway_systems:
                 # create label->index hashes and check if AltLabels duplicated
                 if w.alt_labels[a] in r.pri_label_hash:
                     datacheckerrors.append(DatacheckEntry(r, [w.alt_labels[a]], "DUPLICATE_LABEL"))
-                    r.duplicate_labels.add(a)
+                    r.duplicate_labels.add(w.alt_labels[a])
                 elif w.alt_labels[a] in r.alt_label_hash:
                     datacheckerrors.append(DatacheckEntry(r, [w.alt_labels[a]], "DUPLICATE_LABEL"))
-                    r.duplicate_labels.add(a)
+                    r.duplicate_labels.add(w.alt_labels[a])
                 else:
                     r.alt_label_hash[w.alt_labels[a]] = index
             index += 1
