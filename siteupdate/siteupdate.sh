48cb638b85224e109bab790c96111b2c10f171b7
jteresco <terescoj@gmail.com>
2023-01-08 17:58:41 -0500

    Check for chcon and mailx, #551

--- siteupdate/siteupdate.sh
+++ siteupdate/siteupdate.sh
@@ -40,6 +40,12 @@ else
     make=make
 fi
 
+# centos sometimes needs a "chcon" command for permissions, we'll run the
+# null command "false" otherwise
+chcon=false
+if hash chcon 2>/dev/null; then
+    chcon=sudo chcon -R --type=httpd_sys_content_t
+fi
 # check default locations of TM web server files
 # NOTE: should check on remote server when remote install
 tmwebdir=/fast/www/tm
@@ -244,19 +250,24 @@ echo "$0: launching $siteupdate"
 $siteupdate -d TravelMapping-$datestr $graphflag -l $indir/$logdir -c $indir/$statdir -g $indir/$graphdir -n $indir/$nmpmdir | tee -a $indir/$logdir/siteupdate.log 2>&1 || exit 1
 date
 
-if [ -x ../nmpfilter/nmpbyregion ]; then
+if [[ -x ../nmpfilter/nmpbyregion ]]; then
     echo "$0: running nmpbyregion"
     ../nmpfilter/nmpbyregion $indir/$logdir/tm-master.nmp $indir/$logdir/nmpbyregion/
 else
-    echo "$0: SKIPPING nmpbyregion (../../nmpfilter/nmpbyregion not executable)"
+    echo "$0: SKIPPING nmpbyregion (no ../nmpfilter/nmpbyregion executable)"
 fi
 
-if [ "$install" == "0" ]; then
+if [[ "$install" == "0" ]]; then
     echo "$0: SKIPPING file copies and DB update"
     exit 0
 fi
 
-if [ "$graphflag" != "-k" ]; then
+if ! hash mysql 2>/dev/null; then
+    echo "$0: No mysql program, SKIPPING file copies and DB update"
+    exit 1
+fi
+
+if [[ "$graphflag" != "-k" ]]; then
     echo "$0: creating zip archive of all graphs"
     cd $indir/$graphdir
     zip -q graphs.zip *.tmg
@@ -270,7 +281,7 @@ touch $tmwebdir/dbupdating
 echo "$0: loading primary DB"
 date
 mysql --defaults-group-suffix=tmapadmin -u travmapadmin TravelMapping < TravelMapping-$datestr.sql
-if [ -d $tmwebdir/$grapharchives ]; then
+if [[ -d $tmwebdir/$grapharchives ]]; then
     for archive in $tmwebdir/$grapharchives/*/*.sql; do
 	mysql --defaults-group-suffix=tmapadmin -u travmapadmin TravelMapping < $archive
     done
@@ -285,31 +296,38 @@ ln -sf $tmwebdir/lib/tm.conf.standard $tmwebdir/lib/tm.conf
 echo "$0: installing logs, stats, nmp_merged, graphs, archiving old contents in $tmarchivedir/$datestr"
 mkdir -p $tmarchivedir/$datestr
 mv $tmwebdir/$logdir $tmarchivedir/$datestr
-mv $datestr/$logdir $tmwebdir
+mv $indir/$logdir $tmwebdir
+$chcon $tmwebdir/$logdir
 mv $tmwebdir/$statdir $tmarchivedir/$datestr
-mv $datestr/$statdir $tmwebdir
+mv $indir/$statdir $tmwebdir
+$chcon $tmwebdir/$statdir
 mv $tmwebdir/$nmpmdir $tmarchivedir/$datestr
-mv $datestr/$nmpmdir $tmwebdir
-if [ "$graphflag" != "-k" ]; then
+mv $instr/$nmpmdir $tmwebdir
+if [[ "$graphflag" != "-k" ]]; then
     mv $tmwebdir/$graphdir $tmarchivedir/$datestr
-    mv $datestr/$graphdir $tmwebdir
+    mv $indir/$graphdir $tmwebdir
+    $chcon $tmwebdir/$graphdir
 fi
-rmdir $datestr
+rmdir $indir
 
 echo "$0: loading DB copy"
 mysql --defaults-group-suffix=tmapadmin -u travmapadmin TravelMappingCopy < TravelMapping-$datestr.sql
-if [ -d $tmwebdir/$grapharchives ]; then
+if [[ -d $tmwebdir/$grapharchives ]]; then
     for archive in $tmwebdir/$grapharchives/*/*.sql; do
 	mysql --defaults-group-suffix=tmapadmin -u travmapadmin TravelMappingCopy < $archive
     done
 fi
 echo "$0: moving sql file to archive"
 mv TravelMapping-$datestr.sql $tmarchivedir
-echo "$0: sending email notification"
-mailx -s "Travel Mapping Site Update Complete" travelmapping-siteupdates@teresco.org <<EOF
+if hash mailx 2>/dev/null; then
+    echo "$0: sending email notification"
+    mailx -s "Travel Mapping Site Update Complete" travelmapping-siteupdates@teresco.org <<EOF
 A Travel Mapping site update has just successfully completed.
 The complete log is available at https://travelmapping.net/logs/siteupdate.log .
 Please report any problems to travmap@teresco.org .
 EOF
+else
+    echo "$0: no mailx program, skipping email notification"
+fi
 echo "$0: complete"
 date
